d9045970b2387d0d4aaa653276572b533c45d916
LilGast8 <contact@gastonbouchayer.com>
2014-07-15 17:04:36 +0200

    dat.GUI : clean disable/enable mode

--- www/src/js/app/com/config/DatGui.js
+++ www/src/js/app/com/config/DatGui.js
@@ -18,19 +18,19 @@ APP.DatGui = (function(window){
 		var background = this.gui.add(APP.Views.Index, 'backgroundColor', ['Dark', 'Light', 'EpilepticKiller']);
 		var colorization = this.gui.add(APP.Views.Index, 'colorizationMode', ['FromGreyToColorToGrey', 'FromGreyToColor', 'FromColorToColor']);
 		var camera = this.gui.add(APP.Views.Index, 'cameraMode', ['Perspective3D', 'Flat3D']);
-	//	var light = this.gui.add(APP.Views.Index, 'lightMode', ['GlobalLight', 'PartialLight', 'TheyComeFromDarkness']);
-		this.light = this.gui.add(APP.Views.Index, 'lightMode', ['GlobalLight', 'PartialLight', 'TheyComeFromDarkness']);
+		var light = this.gui.add(APP.Views.Index, 'lightMode', ['GlobalLight', 'PartialLight', 'TheyComeFromDarkness']);
 		
 		changeXRows.onFinishChange(APP.Views.Index.updateCubes.bind(APP.Views.Index));
 		changeYRows.onFinishChange(APP.Views.Index.updateCubes.bind(APP.Views.Index));
 		background.onChange(APP.Views.Index.changeBackground.bind(APP.Views.Index));
 		colorization.onChange(_manageColorization.bind(this));
-	//	camera.onChange(APP.Views.Index.changeView.bind(APP.Views.Index));
 		camera.onChange(_manageCamera.bind(this));
-		this.light.onChange(_manageLight.bind(this));
+		light.onChange(_manageLight.bind(this));
 		
-		this.$.bgcModeSelect = $('.dg .main ul li:nth-child(3) select');
-		this.$.lightModeSelect = $('.dg .main ul li:nth-child(6) select');
+		this.bgcModeController = this.gui.__controllers[2];
+		this.bgcModeSelect = this.bgcModeController.__select;
+		this.lightModeController = this.gui.__controllers[5];
+		this.lightModeSelect = this.lightModeController.__select;
 		
 		_disabledLightSelect.call(this);
 	};
@@ -44,13 +44,10 @@ APP.DatGui = (function(window){
 	
 	var _manageCamera = function(v) {
 		if(v == 'Perspective3D') {
-			if(this.$.lightModeSelect[0].selectedIndex == 1) {
-				this.$.lightModeSelect[0].selectedIndex = 0;
-				APP.Views.Index.changeLight('GlobalLight');
-			}
+			if(this.lightModeController.getValue() != 'GlobalLight') this.lightModeController.setValue('GlobalLight');
 			_disabledLightSelect.call(this);
 		}
-		else if(v == 'Flat3D') _enabledLightSelect.call(this);
+		else _enabledLightSelect.call(this);
 		
 		APP.Views.Index.changeView(v);
 	};
@@ -58,32 +55,8 @@ APP.DatGui = (function(window){
 	
 	var _manageLight = function(v) {
 		if(v == 'TheyComeFromDarkness') {
-			console.log(this.gui.__controllers[2]);
-			console.log(this.gui.__controllers[2].options);
-			console.log(this.gui.__controllers[2].__select);
-			
-			if(this.gui.__controllers[2].getValue() != 'Dark') this.gui.__controllers[2].setValue('Dark');
-			
-		//	this.gui.__controllers[2].__select.disabled = true;
+			if(this.bgcModeController.getValue() != 'Dark') this.bgcModeController.setValue('Dark');
 			_disabledBgcSelect.call(this);
-			
-			/*
-			if(this.$.bgcModeSelect[0].selectedIndex !== 0) {
-				console.log('bg dark');
-				
-			//	this.$.bgcModeSelect[0].selectedIndex = 0;
-				
-			//	console.log(this.light);
-			//	this.light.updateDisplay();
-			//	console.log(this.gui.__controllers);
-			//	this.gui.__controllers[2].setValue();
-				
-				
-			//	APP.Views.Index.changeBackground('Dark');
-			//	light.onChange();
-			}
-			*/
-			
 		}
 		else _enabledBgcSelect.call(this);
 		
@@ -92,24 +65,22 @@ APP.DatGui = (function(window){
 	
 	
 	var _disabledBgcSelect = function() {
-	//	this.$.bgcModeSelect[0].disabled = true;
-		this.gui.__controllers[2].__select.disabled = true;
+		this.bgcModeSelect.disabled = true;
 	};
 	
 	
 	var _enabledBgcSelect = function() {
-	//	this.$.bgcModeSelect[0].disabled = false;
-		this.gui.__controllers[2].__select.disabled = false;
+		this.bgcModeSelect.disabled = false;
 	};
 	
 	
 	var _disabledLightSelect = function() {
-		this.$.lightModeSelect[0].disabled = true;
+		this.lightModeSelect.disabled = true;
 	};
 	
 	
 	var _enabledLightSelect = function() {
-		this.$.lightModeSelect[0].disabled = false;
+		this.lightModeSelect.disabled = false;
 	};
 	
 	
